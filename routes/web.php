422a14e4e92390180bec8ebbf5f519a8633a360f
Abdullah TATLICAK <tatlicak@gmail.com>
2023-11-19 16:53:49 +0300

    Creating first pages

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\IndexController;
 
 /*
 |--------------------------------------------------------------------------
@@ -12,4 +13,5 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
+ Route::get('/', [IndexController::class,'index'])->name('index');
+ Route::get('show', [IndexController::class,'show'])->name('show');
