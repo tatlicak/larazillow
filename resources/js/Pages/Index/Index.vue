d202d88f1645646461943e3b647eaaa5ee4b3ce5
Abdullah TATLICAK <tatlicak@gmail.com>
2023-11-22 06:28:05 +0300

    Default layouts

--- resources/js/Pages/Index/Index.vue
+++ resources/js/Pages/Index/Index.vue
@@ -15,11 +15,3 @@
         'message':String
     });
 </script>
-
-<script>
-    import MainLayout from '../../Layouts/MainLayout.vue'
-    export default {
-        layout: MainLayout
-    }
-
-</script>
--- resources/js/Pages/Index/Show.vue
+++ resources/js/Pages/Index/Show.vue
@@ -9,11 +9,3 @@
     import { Link } from '@inertiajs/vue3'
    
 </script>
-
-<script>
-    import MainLayout from '../../Layouts/MainLayout.vue'
-    export default {
-        layout: MainLayout
-    }
-
-</script>
