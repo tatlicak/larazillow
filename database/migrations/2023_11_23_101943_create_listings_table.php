2a69b7b421fdad8ce0cc86f9544b8bbcead33d3e
Abdullah TATLICAK <tatlicak@gmail.com>
2023-11-24 05:06:44 +0300

    Model Factories & Seeders

--- database/migrations/2023_11_23_101943_create_listings_table.php
+++ database/migrations/2023_11_23_101943_create_listings_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->unsignedTinyInteger('beds');
             $table->unsignedTinyInteger('baths');
-            $table->unsignedTinyInteger('area');
+            $table->unsignedSmallInteger('area');
 
             $table->tinyText('city');
             $table->tinyText('code');
