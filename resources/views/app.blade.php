535115f908d36ecf8f204cb5b55cb3066523da5d
Abdullah TATLICAK <tatlicak@gmail.com>
2024-01-17 13:09:26 +0300

    57. Installing and Setting Up Tailwind CSS

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -9,7 +9,7 @@
         @vite('resources/js/app.js')
         @inertiaHead  
     </head>
-    <body>
+    <body class = "bg-white dark:bg-gray-900 dark:text-white">
        @inertia
     </body>
 </html>
