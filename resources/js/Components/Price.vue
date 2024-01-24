c46c236a040ad7dbf646728bbc9f8419e9955c34
Abdullah TATLICAK <tatlicak@gmail.com>
2024-01-24 10:21:28 +0300

    62. Custom Price Formatting Component

---
+++ resources/js/Components/Price.vue
@@ -0,0 +1,22 @@
+<template>
+    <span>
+            {{ formattedPrice }}
+    </span>
+</template>
+
+<script setup>
+import {computed} from 'vue'
+
+const props = defineProps({
+    price: [Number, String],
+})
+
+const formattedPrice = computed(
+    () => Number(props.price).toLocaleString('en-US', {
+        style: 'currency',
+        currency: 'USD',
+        maximumFracrionDigits: 2,
+
+    }),
+)
+</script>
