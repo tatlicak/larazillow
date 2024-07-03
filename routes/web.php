8816264d2a9b796242d21b05eb30d65c4ba7cda9
Abdullah TATLICAK <tatlicak@gmail.com>
2024-07-03 11:45:16 +0300

    77. User Registration Form

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\IndexController;
 use App\Http\Controllers\ListingController;
+use App\Http\Controllers\UserAccountController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,3 +24,5 @@ use App\Http\Controllers\ListingController;
  Route::get('login', [AuthController::class, 'create'])->name('login');
  Route::post('login', [AuthController::class, 'store'])->name('login.store');
 Route::delete('logout', [AuthController::class, 'destroy'])->name('logout');
+
+Route::resource('user-account', UserAccountController::class)->only(['create']);
