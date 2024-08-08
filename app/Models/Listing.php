ba0051289c21a4a6df25b42d96bf6c666a4d0d0d
Abdullah TATLICAK <tatlicak@gmail.com>
2024-08-08 04:03:23 +0300

    85. Creating a One-to-Many Database Relationship

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -4,10 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Listing extends Model
 {
     use HasFactory;
 
     protected $guarded = [];
+
+    public function owner(): BelongsTo
+    {
+        return $this->belongsTo(\App\Models\User::class, 'by_user_id');
+    }
 }
