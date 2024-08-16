8c97a5c3ed261bee03c5c9a7c014f7891b0ac2f1
Abdullah TATLICAK <tatlicak@gmail.com>
2024-08-16 07:48:51 +0300

    90. Model Policies - In Practice

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -4,9 +4,14 @@ namespace App\Http\Controllers;
 
 use App\Models\Listing;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ListingController extends Controller
 {
+    public function __construct()
+    {
+        $this->authorizeResource(Listing::class, 'listing');
+    }
     /**
      * Display a listing of the resource.
      */
@@ -58,6 +63,11 @@ class ListingController extends Controller
      */
     public function show(Listing $listing)
     {
+       /*  if(Auth::user()->cannot('view',$listing)) {
+            abort(403);
+        } */
+
+        $this->authorize('view',$listing);
         return inertia(
             'Listing/Show',
             [
